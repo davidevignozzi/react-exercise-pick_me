c21f824707e5721265e9563cf363ff198ae4c896
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-10-14 10:38:39 +0200

    Routes

--- src/screen/paginated-screen.js
+++ src/screen/paginated-screen.js
@@ -1,13 +1,14 @@
-import React, { useCallback, useEffect, useState } from "react";
-import Layout from "../components/layout";
-import { useSelector, useDispatch } from "react-redux";
-import PhotoSection from "../components/Photo-Section";
-import Paginator from "../components/Paginator";
+import React, { useCallback, useEffect, useState } from 'react';
+import Layout from '../components/layout';
+import { useSelector, useDispatch } from 'react-redux';
+import PhotoSection from '../components/Photo-Section';
+import Paginator from '../components/Paginator';
 // import { Container, Stack } from "../components/styled";
 // import { rowalizer } from "../utils/helpers";
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 const PaginatedScreen = () => {
+  const { page } = useParams;
   return (
     <Layout>
       <h1>Paginated</h1>
