aafcb578b2638563d9345803b91a7c543f4297bf
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-10-14 10:52:32 +0200

    Styled System test

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -1,8 +1,9 @@
 // import InputWrapper from "./Input";
 // import Container from "./Container";
-// import Box from "./Box";
+import Box from './Box';
 // import Button from "./Button";
 // import Stack from "./Stack";
 // import Skeleton from "./Skeleton";
 
+export { Box };
 // export { InputWrapper, Container, Box, Button, Stack, Skeleton };
--- src/screen/home-screen.js
+++ src/screen/home-screen.js
@@ -1,10 +1,20 @@
-import React from "react";
-import Layout from "../components/layout";
+import React from 'react';
+import Layout from '../components/layout';
+import { Box } from '../components/styled';
 
 const HomePage = () => {
   return (
     <Layout>
       <h1>Home</h1>
+      <Box
+        width="400px"
+        height="239px"
+        border="1px solid"
+        borderColor="red"
+        borderRadious="16px"
+        m="10px"
+        bg="green"
+      ></Box>
     </Layout>
   );
 };
