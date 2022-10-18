47b95356d20fa52f7ba530f4e5400536108e9269
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-10-18 10:44:54 +0200

    Paginated Screen pt 2

--- src/screen/paginated-screen.js
+++ src/screen/paginated-screen.js
@@ -26,7 +26,7 @@ const PaginatedScreen = () => {
   const fetchPaginatedData = useCallback(() => {
     dispatch(updatePage(page));
     dispatch(
-      fetchData(`${path}per_page${item_per_page}&page=${page}`)
+      fetchData(`${path}per_page=${item_per_page}&page=${page}`)
     );
     dispatch(
       saveQuery({ query, path, type, itemPerPage: item_per_page })
@@ -39,7 +39,44 @@ const PaginatedScreen = () => {
 
   return (
     <Layout>
-      <h1>Paginated page: {page}</h1>
+      <Container mt="72px">
+        <Stack direction="column" spacing="118px">
+          {(!loading && !error.status && photos?.length > 0) ||
+          photos?.results?.length > 0 ? (
+            rowalizer(photos?.results ? photos.results : photos).map(
+              (row, index) => <PhotoSection row={row} key={index} />
+            )
+          ) : !loading && error.status ? (
+            <h3>
+              {error?.message && error?.message?.length > 0
+                ? error.message.join(' ')
+                : 'Sorry, There was an Error. Try Later'}
+            </h3>
+          ) : (
+            <h3>Loading ...</h3>
+          )}
+
+          <Stack justify="flex-end">
+            <p style={{ color: 'var(--grey-700)' }}>
+              Item per Page
+              <select
+                value={item_per_page}
+                onChange={(e) => set_item_per_page(e.target.value)}
+              >
+                {Array.from({ length: 7 }, (_, index) => {
+                  return (index + 1) * 3;
+                }).map((el) => {
+                  return (
+                    <option value={el} key={`option-${el}`}>
+                      {el}
+                    </option>
+                  );
+                })}
+              </select>
+            </p>
+          </Stack>
+        </Stack>
+      </Container>
       <Paginator />
     </Layout>
   );
